883bdde847aa165247efc10c6379e482783a6d44
Solal Rapaport <solal.rapaport@telecom-paris.fr>
2025-09-17 12:06:22 +0000

    script to empty database and cache before running again

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm examples/configs/current_repo.yml
+rm data/repodata/*
+rm data/paper/repodata_directed/*
+rm data/paper/repodata_undirected/*
+rm data/paper/repodata_undirected_anom/*
+rm working/graphdata/*
+rm -r tmp/*
